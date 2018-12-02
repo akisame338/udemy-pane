fa7dc018fd5866912caab14cd91c09d669c64702
akisame338 <akisame338@gmail.com>
2018-12-02 17:10:20 +0900

    first commit

---
+++ spec/udemy-pane-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import UdemyPaneView from '../lib/udemy-pane-view';
+
+describe('UdemyPaneView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
